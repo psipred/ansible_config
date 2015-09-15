db112eec5f757e6043ab8c267169b97e1db87178
Daniel Buchan <clone@myself.com>
2015-09-15 17:14:57 +0100

    adding some bits and pieces

--- ansible_set_up.sh
+++ ansible_set_up.sh
@@ -1,3 +1,6 @@
+# switch to the relevant virtualenv
+# source /scratch0/NOT_BACKED_UP/dbuchan/python2/bin/activate.csh
+
 sudo easy_install pip
 sudo pip install paramiko PyYAML Jinja2 httplib2 six
 git clone git://github.com/ansible/ansible.git --recursive
@@ -8,10 +11,10 @@ export ANSIBLE_INVENTORY=~/ansible_hosts
 
 # scp ~/.ssh/rsa_id.pub dbuchan@bioinfstageX:/home/dbuchan/.ssh/authorized_keys
 
-# on target machines add 
+# on target machines add
 # dbuchan ALL=(ALL) NOPASSWD: ALL
 # to visudo config
 
 # ssh-agent bash
 # ssh-add ~/.ssh/id_rsa
-
+thisisit
