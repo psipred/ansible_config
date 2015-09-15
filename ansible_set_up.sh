942cb843e6733039db12f7170ae2fdc8bc0971e5
DanBuchan <clone@myself.com>
2015-09-15 12:41:32 +0100

    moved setup things to here

---
+++ ansible_set_up.sh
@@ -0,0 +1,17 @@
+sudo easy_install pip
+sudo pip install paramiko PyYAML Jinja2 httplib2 six
+git clone git://github.com/ansible/ansible.git --recursive
+cd ./ansible
+source ./hacking/env-setup
+echo "127.0.0.1" > ~/ansible_hosts
+export ANSIBLE_INVENTORY=~/ansible_hosts
+
+# scp ~/.ssh/rsa_id.pub dbuchan@bioinfstageX:/home/dbuchan/.ssh/authorized_keys
+
+# on target machines add 
+# dbuchan ALL=(ALL) NOPASSWD: ALL
+# to visudo config
+
+# ssh-agent bash
+# ssh-add ~/.ssh/id_rsa
+
