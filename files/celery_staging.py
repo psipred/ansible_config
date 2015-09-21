ade3e31a8225104d902d3bf739890ddf77da5f34
Daniel Buchan <clone@myself.com>
2015-09-21 16:43:47 +0100

    adding celery.py

---
+++ files/celery_staging.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+import os
+import time
+
+from kombu import Exchange, Queue
+from celery import Celery
+from django.conf import settings
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'analytics_automated_project.settings.dev')
+
+
+# logger = get_task_logger(__name__)
+app = Celery('backend_tasks')
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+CELERY_QUEUES = (
+    Queue('localhost', routing_key='localhost.#'),
+    Queue('RServe', routing_key='RServe.#'),
+    Queue('GridEngine', routing_key='GridEngine.#'),
+)
