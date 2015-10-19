0949dde514bc23e50b927bfbd914fddf6f6abc0f
Daniel Buchan <clone@myself.com>
2015-10-19 11:38:12 +0100

    local change

--- files/wsgi.py
+++ files/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import logging
+import sys
+logging.basicConfig(stream=sys.stderr)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics_automated_project.settings.staging")
 
