1d210664f9fcfb5be169201c222b48989ee17959
Daneil Buchan <clone@myself.com>
2017-03-14 15:35:04 +0000

    minor set up update

--- ansible_set_up.sh
+++ ansible_set_up.sh
@@ -1,10 +1,10 @@
-sudo easy_install pip
-sudo pip install paramiko PyYAML Jinja2 httplib2 six
-git clone git://github.com/ansible/ansible.git --recursive
-cd ./ansible
-source ./hacking/env-setup
-echo "127.0.0.1" > ~/ansible_hosts
-export ANSIBLE_INVENTORY=~/ansible_hosts
+# sudo easy_install pip
+# sudo pip install paramiko PyYAML Jinja2 httplib2 six
+# git clone git://github.com/ansible/ansible.git --recursive
+# cd ./ansible
+# source ./hacking/env-setup
+# echo "127.0.0.1" > ~/ansible_hosts
+# export ANSIBLE_INVENTORY=~/ansible_hosts
 
 # scp ~/.ssh/rsa_id.pub dbuchan@bioinfstageX:/home/dbuchan/.ssh/authorized_keys
 
@@ -12,14 +12,14 @@ export ANSIBLE_INVENTORY=~/ansible_hosts
 # dbuchan ALL=(ALL) NOPASSWD: ALL
 # to visudo config
 
-# ssh-agent bash
-# ssh-add ~/.ssh/id_rsa
+ssh-agent bash
+ssh-add ~/.ssh/id_rsa
 # switch to the relevant virtualenv
-# source /scratch0/NOT_BACKED_UP/dbuchan/python2/bin/activate
-# source /cs/research/bioinf/home1/green/dbuchan/ansible/hacking/env-setup
+source /scratch0/NOT_BACKED_UP/dbuchan/python2/bin/activate
+source /home/dbuchan/Applications/ansible-2.2.1.0-1/hacking/env-setup
 
 #Everything
 ansible-playbook -i staging deploy_staging.yml
 
 webservices
-ansible-playbook -i staging restart_aa.yml
+# ansible-playbook -i staging restart_aa.yml
